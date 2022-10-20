9cb089fe36dc06fe59e04d4749e556b8f20fdc82
boindil <boindil@users.noreply.github.com>
2022-10-20 19:41:42 +0200

    minor changes to adjust to original project state

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    presets: [ '@babel/preset-env' ],
-    env: {
-        test: {
-            plugins: [ '@babel/plugin-transform-runtime' ],
-        },
-    },
-};
+    presets: ['@babel/preset-env'],
+    plugins: [['@babel/transform-runtime']],
+  }
+  
---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    testRegex: 'spec.js$',
+    moduleFileExtensions: ['js', 'vue', 'ts'],
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.ts$': 'ts-jest',
+        '.*\\.(vue)$': 'vue3-jest',
+    },
+    coverageDirectory: './coverage/',
+    testEnvironmentOptions: {
+        pretendToBeVisual: true,
+    },
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./tests/setup.ts'],
+    moduleNameMapper: {},
+}
